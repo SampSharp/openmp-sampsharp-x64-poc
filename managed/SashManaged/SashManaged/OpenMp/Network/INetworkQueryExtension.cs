cf7f69be3694f5089a01c66cc64944da3c1e49ed
Tim Potze <timpotze@gmail.com>
2024-02-17 21:07:59 +0100

    fix typo

--- managed/SashManaged/SashManaged/OpenMp/Network/INetworkQueryExtension.cs
+++ managed/SashManaged/SashManaged/OpenMp/Network/INetworkQueryExtension.cs
@@ -6,6 +6,6 @@ public readonly partial struct INetworkQueryExtension
     public static UID ExtensionId => new(0xfd46e147ea474971);
 
     public partial bool AddRule(StringView rule, StringView value);
-    public partial bool RemvoeRule(StringView rule);
+    public partial bool RemoveRule(StringView rule);
     public partial bool IsValidRule(StringView rule);
 }
