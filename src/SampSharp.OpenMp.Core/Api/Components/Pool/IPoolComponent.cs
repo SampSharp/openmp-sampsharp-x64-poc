ec211c6af46804c600c0bd6bd62a14f9a88317ed
Tim Potze <timpotze@gmail.com>
2025-02-21 00:49:38 +0100

    cleanup

--- src/SampSharp.OpenMp.Core/Api/Components/Pool/IPoolComponent.cs
+++ src/SampSharp.OpenMp.Core/Api/Components/Pool/IPoolComponent.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.InteropServices;
-
-namespace SampSharp.OpenMp.Core.Api;
+﻿namespace SampSharp.OpenMp.Core.Api;
 
 [OpenMpApi(typeof(IComponent))]
 [OpenMpApiPartial]
@@ -10,10 +8,4 @@ public readonly partial struct IPoolComponent<T> where T : unmanaged, IIDProvide
     {
         return new IPool<T>(IPoolComponentInterop.cast_IPoolComponent_to_IPool(_handle));
     }
-}
-
-internal static class IPoolComponentInterop
-{
-    [DllImport("SampSharp", EntryPoint = "cast_IPoolComponent_to_IPool")]
-    public static extern nint cast_IPoolComponent_to_IPool(nint handle);
 }
