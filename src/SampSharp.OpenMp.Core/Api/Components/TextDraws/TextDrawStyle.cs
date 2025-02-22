e2cfd24d77da5bf46494a0c09c2807fd7ad04ea1
Tim Potze <timpotze@gmail.com>
2025-02-22 15:41:26 +0100

    configure builds

--- src/SampSharp.OpenMp.Core/Api/Components/TextDraws/TextDrawStyle.cs
+++ src/SampSharp.OpenMp.Core/Api/Components/TextDraws/TextDrawStyle.cs
@@ -2,12 +2,6 @@
 
 public enum TextDrawStyle
 {
-    _0, //< Font type
-    _1, //< Font type
-    _2, //< Font type
-    _3, //< Font type
-    _4, //< TXD sprite
-    _5, //< Model preview
     FontBeckettRegular = 0, //< Font type
     FontAharoniBold, //< Font type
     FontBankGothic, //< Font type
