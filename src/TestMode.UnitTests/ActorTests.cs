f057c872207a7f7cacd533908e0fe48f90848b67
Tim Potze <timpotze@gmail.com>
2025-03-08 14:32:05 +0100

    Added first vehicle test and test cleanup

--- src/TestMode.UnitTests/ActorTests.cs
+++ src/TestMode.UnitTests/ActorTests.cs
@@ -15,13 +15,13 @@ public class ActorTests : TestSystem
     }
 
     [TestSetup]
-    public void CreateTestActor()
+    public void Setup()
     {
         _actor = _worldService.CreateActor(45, new Vector3(4, 5, 6), 45);
     }
 
     [TestCleanup]
-    public void DestroyTestActor()
+    public void Cleanup()
     {
         _actor?.Destroy();
     }
@@ -38,48 +38,55 @@ public class ActorTests : TestSystem
     }
     
     [Test]
-    public void Actor_Position_roundtrip()
+    public void Angle_should_roundtrip()
+    {
+        _actor.Angle = 88;
+        _actor.Angle.ShouldBe(88);
+    }
+
+    [Test]
+    public void Position_should_roundtrip()
     {
         _actor.Position = new Vector3(1, 2, 3);
         _actor.Position.ShouldBe(new Vector3(1, 2, 3));
     }
     
     [Test]
-    public void Health_roundtrip()
+    public void Health_should_roundtrip()
     {
         _actor.Health = 94;
         _actor.Health.ShouldBe(94);
     }
 
     [Test]
-    public void IsInvulnerable_roundtrip()
+    public void IsInvulnerable_should_roundtrip_true()
     {
         _actor.IsInvulnerable = true;
         _actor.IsInvulnerable.ShouldBeTrue();
     }
 
     [Test]
-    public void IsInvulnerable_roundtrip_false()
+    public void IsInvulnerable_should_roundtrip_false()
     {
         _actor.IsInvulnerable = false;
         _actor.IsInvulnerable.ShouldBeFalse();
     }
 
     [Test]
-    public void Skin_roundtrip()
+    public void Skin_should_roundtrip()
     {
         _actor.Skin = 99;
         _actor.Skin.ShouldBe(99);
     }
 
     [Test]
-    public void ApplyAnim()
+    public void ApplyAnim_should_succeed()
     {
         _actor.ApplyAnimation("BASEBALL", "Bat_Hit_1", 1, false, false, false, false, 830);
     }
 
     [Test(Environment = TestEnvironment.OnPlayerTrigger)]
-    public void IsStreamedIn()
+    public void IsStreamedIn_should_succeed()
     {
         _actor.IsStreamedIn(Player);
     }
---
+++ src/TestMode.UnitTests/VehicleTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Numerics;
+using SampSharp.Entities.SAMP;
+using Shouldly;
+
+namespace TestMode.UnitTests;
+
+public class VehicleTests : TestSystem
+{
+    private readonly IWorldService _worldService;
+    private Vehicle _vehicle = null!;
+
+    public VehicleTests(IWorldService worldService)
+    {
+        _worldService = worldService;
+    }
+
+    [TestSetup]
+    public void Setup()
+    {
+        _vehicle = _worldService.CreateVehicle(VehicleModelType.Landstalker, new Vector3(10, 0, 5), 30, 5, 8);
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        _vehicle?.Destroy();
+    }
+
+    [Test]
+    public void Position_should_roundtrip()
+    {
+        _vehicle.Position = new Vector3(1, 2, 3);
+        _vehicle.Position.ShouldBe(new Vector3(1, 2, 3));
+    }
+}
