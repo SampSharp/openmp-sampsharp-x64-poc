02f815a0fdbe437e43ace89a8b37a1b7e1071d1d
Tim Potze <timpotze@gmail.com>
2025-04-20 22:16:51 +0200

    actors docs

--- src/SampSharp.OpenMp.Core/Api/Components/Actors/IActorsComponent.cs
+++ src/SampSharp.OpenMp.Core/Api/Components/Actors/IActorsComponent.cs
@@ -11,7 +11,18 @@ public readonly partial struct IActorsComponent
     /// <inheritdoc />
     public static UID ComponentId => new(0xc81ca021eae2ad5c);
 
+    /// <summary>
+    /// Gets the event dispatcher for actor-related events.
+    /// </summary>
+    /// <returns>An event dispatcher for actor-related events.</returns>
     public partial IEventDispatcher<IActorEventHandler> GetEventDispatcher();
 
+    /// <summary>
+    /// Creates a new actor in the game world.
+    /// </summary>
+    /// <param name="skin">The skin ID of the actor.</param>
+    /// <param name="pos">The position where the actor will be created.</param>
+    /// <param name="angle">The facing angle of the actor.</param>
+    /// <returns>The created actor instance.</returns>
     public partial IActor Create(int skin, Vector3 pos, float angle);
 }
