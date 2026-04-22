f222f8e8f2796c37212706591f5587a39f623806
Borys Stelmakh <bs@vortalcoil.com>
2026-04-22 20:12:14 +0400

    Refactor player entity handling

--- src/SampSharp.OpenMp.Entities/Entities/EntityId.cs
+++ src/SampSharp.OpenMp.Entities/Entities/EntityId.cs
@@ -1,83 +1,64 @@
-﻿namespace SampSharp.Entities;
+namespace SampSharp.Entities;
 
 /// <summary>Represents an identifier of an entity.</summary>
 public readonly record struct EntityId
 {
-    private readonly Guid _id;
-
     /// <summary>An empty entity identifier.</summary>
     public static readonly EntityId Empty = new();
 
-    /// <summary>Initializes a new instance of the <see cref="EntityId" /> struct.</summary>
-    /// <param name="id">The id</param>
+    private readonly Guid _id;
+
     private EntityId(Guid id)
     {
         _id = id;
     }
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="EntityId" /> struct.
-    /// </summary>
-    /// <returns></returns>
+    /// <summary>Gets a value indicating whether this handle is empty.</summary>
+    public bool IsEmpty => _id == Guid.Empty;
+
+    /// <summary>Creates a fresh entity handle backed by a new <see cref="Guid" />.</summary>
     public static EntityId NewEntityId()
     {
         return new EntityId(Guid.NewGuid());
     }
 
-    /// <summary>Gets a value indicating whether this handle is empty.</summary>
-    public bool IsEmpty => _id == Guid.Empty;
-    
     /// <inheritdoc />
     public override string ToString()
     {
-        return IsEmpty
-            ? "(Empty)"
-            : $"(Id = {_id})";
+        return IsEmpty ? "(Empty)" : $"(Id = {_id})";
     }
 
-    /// <summary>Performs an implicit conversion from <see cref="Component" /> to <see cref="EntityId" />. Returns the
-    /// entity of the component.</summary>
-    /// <param name="component">The component.</param>
-    /// <returns>The entity of the component.</returns>
+    /// <summary>
+    ///     Performs an implicit conversion from <see cref="Component" /> to <see cref="EntityId" />.
+    ///     Returns the entity of the component.
+    /// </summary>
     public static implicit operator EntityId(Component component)
     {
         return component?.Entity ?? default;
     }
 
     /// <summary>
-    /// Performs an implicit conversion from <see cref="EntityId" /> to <see cref="bool" />.  Returns <see langword="true" /> if the
-    /// specified <paramref name="value" /> is not empty.
+    ///     Performs an implicit conversion from <see cref="EntityId" /> to <see cref="bool" />.
+    ///     Returns <see langword="true" /> if the specified <paramref name="value" /> is not empty.
     /// </summary>
-    /// <param name="value">The value.</param>
-    /// <returns><see langword="true" /> if the specified <paramref name="value" /> is not empty; otherwise, <see langword="false" />.</returns>
     public static implicit operator bool(EntityId value)
     {
         return !value.IsEmpty;
     }
 
-    /// <summary>
-    /// Implements the operator <see langword="true" />. Returns <see langword="true" /> if the specified <paramref name="value" /> is not
-    /// empty.
-    /// </summary>
-    /// <param name="value">The value.</param>
-    /// <returns><see langword="true" /> if the specified <paramref name="value" /> is not empty; otherwise, <see langword="false" />.</returns>
+    /// <summary>Implements the operator <see langword="true" />.</summary>
     public static bool operator true(EntityId value)
     {
         return !value.IsEmpty;
     }
 
-    /// <summary>Implements the operator <see langword="false" />. Returns <see langword="true" /> if the specified <paramref name="value" /> is
-    /// empty.</summary>
-    /// <param name="value">The value.</param>
-    /// <returns><see langword="true" /> if the specified <paramref name="value" /> is empty; otherwise, <see langword="false" />.</returns>
+    /// <summary>Implements the operator <see langword="false" />.</summary>
     public static bool operator false(EntityId value)
     {
         return value.IsEmpty;
     }
 
-    /// <summary>Implements the operator <c>!</c>. Returns <see langword="true" /> if the specified <paramref name="value" /> is empty.</summary>
-    /// <param name="value">The value.</param>
-    /// <returns><see langword="true" /> if the specified <paramref name="value" /> is empty; otherwise, <see langword="false" />.</returns>
+    /// <summary>Implements the operator <c>!</c>.</summary>
     public static bool operator !(EntityId value)
     {
         return value.IsEmpty;
