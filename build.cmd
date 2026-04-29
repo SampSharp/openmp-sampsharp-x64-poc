c3975d87996948b15267a325b36662ca2f3c8166
Tim Potze <timpotze@gmail.com>
2026-04-29 14:54:41 +0200

    Build w/clang on Windows (#29)

--- build.cmd
+++ build.cmd
@@ -1,4 +1,2 @@
-
-REM We build using MSVC instead of CLANG until this issue has been resolved https://github.com/dotnet/runtime/issues/111788
-cmake -S . -B build -G "Visual Studio 18 2026" -A x64 -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+cmake -S . -B build -A x64 -T ClangCL -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 cmake --build build --config RelWithDebInfo
